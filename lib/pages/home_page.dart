1ae37de8da78c52b701f31fa49fbb40bc2896ac6
AlikKostandov <alik.kostandov.yellow@gmail.com>
2023-05-29 22:54:11 +0300

    @feature - create starting various for home page style

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:event_creater/widgets/event_box.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,37 @@ class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
 }
+
+List<EventBox> events = [
+  const EventBox(
+      color: Colors.grey,
+      name: 'John’s Friday Party',
+      description: 'John Thompson - 27/06/2021'),
+  const EventBox(
+      color: Colors.grey,
+      name: 'John’s Friday Party',
+      description: 'John Thompson - 27/06/2021'),
+  const EventBox(
+      color: Colors.grey,
+      name: 'John’s Friday Party',
+      description: 'John Thompson - 27/06/2021'),
+  const EventBox(
+      color: Colors.grey,
+      name: 'John’s Friday Party',
+      description: 'John Thompson - 27/06/2021'),
+  const EventBox(
+      color: Colors.grey,
+      name: 'John’s Friday Party',
+      description: 'John Thompson - 27/06/2021'),
+  const EventBox(
+      color: Colors.grey,
+      name: 'John’s Friday Party',
+      description: 'John Thompson - 27/06/2021'),
+  const EventBox(
+      color: Colors.grey,
+      name: 'John’s Friday Party',
+      description: 'John Thompson - 27/06/2021')
+];
 
 Future<String?> signOut(BuildContext context) async {
   try {
@@ -28,56 +60,82 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color(0xFFE6E6E6),
-        body: Stack(children: <Widget>[
-          SizedBox(
-            height: _headerHeight,
-            child: HeaderWidget(
-                _headerHeight), //let's create a common header widget
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0, top: 50.0),
-                child: Container(
-                    height: 140.0,
-                    width: 140.0,
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 8.0,
-                          color: const Color(0xFFE6E6E6),
-                        ),
-                        borderRadius: BorderRadius.circular(100)),
-                    child: const CircleAvatar(
-                      backgroundImage: AssetImage('assets/img_avatar.png'),
-                    )),
+        body: Column(
+          children: [
+            Stack(children: <Widget>[
+              SizedBox(
+                height: _headerHeight,
+                child: HeaderWidget(
+                    _headerHeight), //let's create a common header widget
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  // IconButton(
-                  //   onPressed: () async {},
-                  //   icon: const Icon(Icons.menu),
-                  //   color: Colors.black,
-                  //   iconSize: 40,
-                  //   padding: const EdgeInsets.only(top: 50.0),
-                  // ),
-                  IconButton(
-                    onPressed: () async {
-                      await FirebaseAuth.instance.signOut().then((value) {
-                        Navigator.pushNamed(context, "/login");
-                      });
-                      return;
-                    },
-                    icon: const Icon(Icons.logout),
-                    color: Colors.black,
-                    iconSize: 40,
-                    padding: const EdgeInsets.only(top: 50.0, right: 20.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, top: 50.0),
+                    child: Container(
+                        height: 140.0,
+                        width: 140.0,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 10.0,
+                              color: const Color(0xFFE6E6E6),
+                            ),
+                            borderRadius: BorderRadius.circular(100)),
+                        child: const CircleAvatar(
+                          backgroundImage: AssetImage('assets/img_avatar.png'),
+                        )),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () async {
+                          await FirebaseAuth.instance.signOut().then((value) {
+                            Navigator.pushNamed(context, "/login");
+                          });
+                          return;
+                        },
+                        icon: const Icon(Icons.menu),
+                        color: Colors.black,
+                        iconSize: 40,
+                        padding: const EdgeInsets.only(top: 50.0, right: 20.0),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ]));
+            ]),
+            Expanded(
+                child: ListView(
+              children: events,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+            )),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedIconTheme:
+              const IconThemeData(color: Colors.blueAccent, size: 40.0),
+          unselectedIconTheme:
+              const IconThemeData(color: Colors.grey, size: 30.0),
+          backgroundColor: const Color(0xFFE6E6E6),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people_alt),
+              label: 'Communities',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.emoji_events_sharp),
+              label: 'Achievement',
+            ),
+          ],
+        ));
   }
 }

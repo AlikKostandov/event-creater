32ffbb1706c906f4338a1a4136aca239801ae37c
AlikKostandov <alik.kostandov.yellow@gmail.com>
2023-07-06 22:32:25 +0300

    @feature task-36 add swipe-archive for event-box

--- lib/widgets/event_box.dart
+++ lib/widgets/event_box.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../entity/event_entity.dart';
+import 'package:http/http.dart' as http;
 
 // Widget for event
 class EventBox extends StatelessWidget {
@@ -14,6 +15,26 @@ class EventBox extends StatelessWidget {
     required this.onDismissed,
   }) : super(key: key);
 
+  Future<void> removeEvent(int? id) async {
+    if (id != null) {
+      final response = await http.delete(Uri.parse(
+          'http://192.168.1.120:9000/event-creator/events/delete?eventId=$id'));
+      if (response.statusCode != 200) {
+        print('Ошибка: ${response.statusCode}');
+      }
+    }
+  }
+
+  Future<void> archiveEvent(int? id) async {
+    if (id != null) {
+      final response = await http.put(Uri.parse(
+          'http://192.168.1.120:9000/event-creator/events/archive?eventId=$id'));
+      if (response.statusCode != 200) {
+        print('Ошибка: ${response.statusCode}');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,10 +43,26 @@ class EventBox extends StatelessWidget {
         key: Key(event.id.toString()),
         onDismissed: (direction) {
           if (direction == DismissDirection.endToStart) {
-            onDismissed();
+            removeEvent(event.id);
+          } else {
+            archiveEvent(event.id);
           }
         },
         background: Container(
+          decoration: BoxDecoration(
+            color: Colors.blueAccent,
+            border: Border.all(width: 0, color: Colors.grey),
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+          ),
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.only(left: 20.0),
+          child: const Icon(
+            Icons.archive,
+            color: Colors.white,
+            size: 35.0,
+          ),
+        ),
+        secondaryBackground: Container(
           decoration: BoxDecoration(
             color: const Color(0xFF741313),
             border: Border.all(width: 0, color: Colors.grey),
